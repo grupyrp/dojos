a1d81d21d234954749c1873596388646d42098d0
Sergio Oliveira <sergio@tracy.com.br>
2015-07-02 10:15:35 -0300

    Fim do dojo

--- store_credit.py
+++ store_credit.py
@@ -8,12 +8,18 @@ def test_store():
     assert store.credit == 10
     assert store.items == [2, 4]
 
-
 def test_parser():
     parsed = parse_input(input_str) 
     assert isinstance(parsed, list)
     assert isinstance(parsed[0], Store)
+    assert parsed[0].items == [5, 75, 25]
 
+def test_number_cases():
+    number_cases = int(input_str.split('\n')[0])
+    parsed = parse_input(input_str)
+    assert number_cases == len(parsed)
+
+def test_
 
 class Store:
     def __init__(self, credit, items):
@@ -21,6 +27,15 @@ class Store:
         self.items = items
   
 def parse_input(input_str):
-    input_list = input_str.split('\n)')
-    for i, line in enumerate(input_str.split('\n')[1::3]):
-        c = i 
+    input_list = input_str.split('\n')
+    number_cases = int(input_list.pop(0))
+    store_list = []
+    for j in range(number_cases):
+        credit = int(input_list.pop(0))
+        n_items = int(input_list.pop(0))
+        items = map(int, input_list.pop(0).split())
+
+        store_list.append(Store(credit, items))
+
+    return store_list
+        
