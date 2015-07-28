f4ced45993a260e92317a054fd00a9956df8c9be
Marco Rougeth <marco@rougeth.com>
2015-07-28 11:43:28 -0300

    Adiciona dojo do dia 27/07

---
+++ 2015-07-27/README.md
@@ -0,0 +1,32 @@
+Problem
+=======
+Recursive Fibonacci sequence, using cache.
+
+Retrospectiva:
+==============
+
+Positivos:
+----------
+
+* APRENDIZADO!! \o/
+* Cache (lru_cache), escopo, decorators
+* Mais participação
+* Concluímos o problema
+* Evolução do dojo
+* Pós dojo
+
+Negativos:
+----------
+
+* Python2
+* Atraso
+* Muito teste/código com testes quebrando
+* Comunicação piloto/copiloto
+
+Participantes:
+==============
+
+* [rougeth](https://github.com/rougeth)
+* [seocam](https://github.com/seocam)
+* [matheusmf](https://github.com/matheusmf)
+* [rodrigosavian](https://github.com/rodrigosavian)
---
+++ 2015-07-27/fibonacci.py
@@ -0,0 +1,71 @@
+import unittest
+
+
+cache = {}
+count = 0
+
+class TestDojo(unittest.TestCase):
+
+    def setUp(self):
+        clean_cache()
+
+    def test_clean_cache(self):
+        cache[1] = 1
+        clean_cache()
+        assert len(cache) == 0
+
+    def test_fib1(self):
+        assert fib(1) == 1
+
+    def test_fib2(self):
+        assert fib(2) == 1
+
+    def test_fib3(self):
+        assert fib(3) == 2
+
+    def test_fib8(self):
+        assert fib(8) == 21
+
+    def test_cache(self):
+        assert cache == {}
+
+    def test_cache_fib2(self):
+        fib(2)
+        assert cache[2] == 1
+
+    def test_using_cache(self):
+        fib(3)
+        assert count == 4
+
+def clean_cache():
+    cache.clear()
+    global count
+    count = 0
+
+def cachee(f):
+    def fibb(n):
+        if n in cache:
+            return cache[n]
+
+        res = f(n)
+        cache[n] = res
+
+        return res
+
+    return fibb
+
+
+@cachee
+def fib(n):
+    global count
+    count += 1
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib(n-1) + fib(n-2)
+
+if __name__ == '__main__':
+    unittest.main()
