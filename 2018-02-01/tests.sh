de4e4796f20467a9b66fc938648188fa23ef6095
hdelei <hdelei@gmail.com>
2018-02-02 22:10:03 -0200

    Semáforo só é chamado quando o arquivo programa.py é salvo

--- 2018-02-01/tests.sh
+++ 2018-02-01/tests.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
+DEF_TIME=`stat -c %Z programa.py`
+
 while true
 do
-    sleep 1
-    clear
-    python programa.py
-    if [ "$?" = "0" ]; then
-        http 192.168.12.38/gpio/green > /dev/null
-    else
-        http 192.168.12.38/gpio/red > /dev/null
-    fi
+	MOD_TIME=`stat -c %Z programa.py`
+
+	if [[ "$MOD_TIME" != "$DEF_TIME" ]] 
+	then
+	    #echo "programa.py modified"
+	    python programa.py
+	    
+		if [ "$?" = "0" ]
+	    then
+			#echo "green"
+	    	http 192.168.12.38/gpio/green > /dev/null
+	    else
+			#echo "red"	  	
+			http 19.168.12.38/gpio/red > /dev/null
+	    fi
+	    
+	    DEF_TIME=$MOD_TIME
+	fi
+	sleep 1
 done
